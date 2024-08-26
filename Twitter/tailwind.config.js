dce32f052648e83bd9c94017c3b007222c64ae47
Abdullah Qureshi <mabdullahqureshi583@gmail.com>
2024-08-26 18:59:32 +0500

    UI clone of Twitter using TailwindCSS

---
+++ Twitter/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.html"],
+
+  theme: {
+    extend: {
+      screens: {
+        xsm:'500px',
+        
+      },
+    },
+  },
+  plugins: [],
+}
+
